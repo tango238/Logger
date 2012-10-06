4feee1f599fedc5d50f1ee4191591687f3f2929c
Go Tanaka <tanago3@gmail.com>
2012-10-06 19:31:14 +0900

    LogManager内のLoggerContextをAtomicInteger<WeakReference>にした

--- src/main/java/com/plugram/logger/LogManager.java
+++ src/main/java/com/plugram/logger/LogManager.java
@@ -1,9 +1,10 @@
 package com.plugram.logger;
 
-import java.util.Map;
+import java.lang.ref.WeakReference;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicReference;
+
 
 public class LogManager {
 
@@ -11,14 +12,13 @@ public class LogManager {
 	
 	private static LogManager manager;
 	
-	private static Map<String, LoggerContext> contexts = new ConcurrentHashMap<String, LoggerContext>();
+	private static ConcurrentMap<String, AtomicReference<WeakReference<LoggerContext>>> contexts = new ConcurrentHashMap<String, AtomicReference<WeakReference<LoggerContext>>>();
 	
 	static {
 		 manager = new LogManager();
-		 contexts.put(DEFAULT_NAME, new LoggerContext());
+		 WeakReference<LoggerContext> ref = new WeakReference<LoggerContext>(new LoggerContext(DEFAULT_NAME));
+		 contexts.putIfAbsent(DEFAULT_NAME, new AtomicReference<WeakReference<LoggerContext>>(ref));
 	}
-	
-	private Lock lock = new ReentrantLock();
 	
 	public static LogManager getManager() {
 		return manager;
@@ -29,16 +29,14 @@ public class LogManager {
 	}
 	
 	public LoggerContext getLoggerContext(String name) {
-		lock.lock();
-		try {
-			if(contexts.get(name) == null){
-				LoggerContext context = new LoggerContext();
-				contexts.put(name, context);
-				return context;
-			}
-		} finally {
-			lock.unlock();
+		AtomicReference<WeakReference<LoggerContext>> ref = contexts.get(name);
+		if(ref == null){
+			LoggerContext context = new LoggerContext(name);
+			WeakReference<LoggerContext> wr = new WeakReference<LoggerContext>(context);
+			AtomicReference<WeakReference<LoggerContext>> r = new AtomicReference<WeakReference<LoggerContext>>(wr);
+			contexts.putIfAbsent(name, r);
+			return contexts.get(name).get().get();
 		}
-		return contexts.get(name);
+		return contexts.get(name).get().get();
 	}
 }
