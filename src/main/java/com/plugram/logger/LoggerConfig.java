92de20068e20656597c143d7d5a454a242851d7e
Go Tanaka <tanago3@gmail.com>
2012-10-12 19:30:08 +0900

    log()を実装

--- src/main/java/com/plugram/logger/LoggerConfig.java
+++ src/main/java/com/plugram/logger/LoggerConfig.java
@@ -1,5 +1,6 @@
 package com.plugram.logger;
 
+import com.plugram.logger.appender.Appender;
 import com.plugram.logger.config.LogLevel;
 
 public class LoggerConfig {
@@ -10,10 +11,13 @@ public class LoggerConfig {
 	
 	private final LogLevel level;
 	
-	public LoggerConfig(String name, LogLevel level) {
+	private final Appender appender;
+	
+	public LoggerConfig(String name, LogLevel level, Appender appender) {
 		this.name = name;
 		this.logger = newInstance();
 		this.level = level;
+		this.appender = appender;
 	}
 
 	private Logger newInstance() {
@@ -30,6 +34,10 @@ public class LoggerConfig {
 
 	public String getName() {
 		return name;
+	}
+
+	public void log(String msg) {
+		appender.write(msg.getBytes());
 	}
 
 }
--- src/main/java/com/plugram/logger/RootLoggerConfig.java
+++ src/main/java/com/plugram/logger/RootLoggerConfig.java
@@ -1,9 +1,11 @@
 package com.plugram.logger;
 
+import com.plugram.logger.appender.ConsoleAppender;
+
 public class RootLoggerConfig extends LoggerConfig {
 
 	public RootLoggerConfig() {
-		super(null, null);
+		super(null, null, new ConsoleAppender());
 	}
 
 }
